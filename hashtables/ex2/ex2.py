ee7282f9e38148cd5a2a4592be4d0a99c1dd26c2
Rane <github@blasph.me>
2020-02-09 20:16:10 -0700

    Completed hashtables ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,10 +14,24 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = []
+    next = None
 
     """
     YOUR CODE HERE
     """
+    for i in range(0, length):
+        hash_table_insert(hashtable, tickets[i].destination, tickets[i].source)
+
+    for i in range(length):
+        if next is None:
+            route.insert(0, "NONE")
+            next = hash_table_retrieve(hashtable, "NONE")
+        else:
+            next = hash_table_retrieve(hashtable, next)
+        if(next is not "NONE"):
+            route.insert(0, next)
+
+    return route
 
     return route
